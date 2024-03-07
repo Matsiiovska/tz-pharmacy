345c0c9b1c1c97a931ab3a6e04e043cfb4728c19
olia <dovgalyukolya@gmail.com>
2024-03-08 00:35:24 +0300

    modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+//import Layout from './Layout/Layout';
+import { lazy } from 'react';
+import Layout from './Layout/Layout';
+
+const shopPage = lazy(() => import('pages/shopPage/shopPage'));
+const shoppingCartPage = lazy(() =>
+  import('pages/shoppingCartPage/shoppingCartPage')
+);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<shopPage />} />
+        <Route path="shopingcart" element={<shoppingCartPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/pages/shoppingCartPage/shoppingCartPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const shoppingCartPage = () => {
+  return <div>shoppingCartPage</div>;
+};
